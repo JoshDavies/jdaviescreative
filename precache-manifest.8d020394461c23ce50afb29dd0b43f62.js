0dbb016df3c33feb3470890b408c7a1bc6bf9b31
JoshDavies <jdaviescreative@gmail.com>
2020-01-29 17:16:57 +0000

    Updates

---
+++ precache-manifest.8d020394461c23ce50afb29dd0b43f62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0538213f9500e04d0935cf4cae58a1f",
+    "url": "/jdaviescreative/index.html"
+  },
+  {
+    "revision": "76e81cbcfca3c63aa026",
+    "url": "/jdaviescreative/static/css/main.a25bac32.chunk.css"
+  },
+  {
+    "revision": "dc14d78806e6d4af5a6c",
+    "url": "/jdaviescreative/static/js/2.fa3f2e5a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/jdaviescreative/static/js/2.fa3f2e5a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76e81cbcfca3c63aa026",
+    "url": "/jdaviescreative/static/js/main.0d597c62.chunk.js"
+  },
+  {
+    "revision": "92b78a50fc764aca3526",
+    "url": "/jdaviescreative/static/js/runtime-main.95419e8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jdaviescreative/static/media/logo.5d5d9eef.svg"
+  }
+]);
