6cf8f797f5384c88d7fcb7cabba1685812853540
JoshDavies <jdaviescreative@gmail.com>
2020-01-30 23:23:04 +0000

    links to page sections added

--- src/App.js
+++ src/App.js
@@ -9,15 +9,18 @@ function App() {
         <img src="https://github.com/JoshDavies/jdaviescreative/blob/master/public/favicon.png?raw=true" className="Fav"></img>
         <a href="#" className="Title">J Davies</a>
         <nav className="Nav">
-          <a href="#">Latest Work</a>
-          <a href="#">Buy</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
+          <a href="#latest">Latest Work</a>
+          <a href="#buy">Buy</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
           </nav>
           <a href="http://instagram.com/jdaviesartwork" className="fa fa-instagram" target="_blank"></a>
       </header>
       <body className="App-body">
-
+        <h1 id="latest">Latest Work</h1>
+        <h1 id="buy">Buy</h1>
+        <h1 id="about">About</h1>
+        <h1 id="contact">Contact</h1>
       </body>
     </div>
   );
