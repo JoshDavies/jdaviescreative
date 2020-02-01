aae88d37a4810c6c45bd4e5b51149fb7c86e306d
JoshDavies <jdaviescreative@gmail.com>
2020-02-01 01:07:37 +0000

    button improvements

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -10,9 +10,6 @@ function Buy() {
       Based in Somerset House, DegreeArt.com is one of the UK's first Online Art Galleries.
       Over the past 16 years, DegreeArt.com has established itself as the market leader in
       UK student & graduate art sales, hand picking & promoting the most promising talent.
-      <br />
-      <br />
-      For further information, please click the button to visit their website and view my profile:
       </p>
       <a href="https://www.degreeart.com/artists/josh-davies" target="_blank" className="button">J Davies @ DegreeArt.com</a>
     </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,18 @@ function Contact() {
     <div className="page" id="contact">
       <h1>Contact</h1>
       <h3>Get in Touch</h3>
-      <p>Please feel free to email me about my work: jdaviescreative@gmail.com</p>
+      <p>Please feel free to email me about my work: <br />
+      jdaviescreative@gmail.com</p>
       <h3>Commissioning Work</h3>
       <p>
         If you are interested in commissioning Original Art:
-        please inquire through the degreeart.com website (see ‘Buy’ page for further details).
+        please inquire through the degreeart.com website (see <a href="#buy">'Buy'</a> for further details).
       </p>
       <h3>Follow me on Instagram</h3>
-      <p>@jdaviesartwork</p>
-      <a href="http://instagram.com/jdaviesartwork" className="fa fa-instagram" target="_blank"></a>
+      <p>
+        For more photographs and close ups:
+      </p>
+        <a href="http://instagram.com/jdaviesartwork" target="_blank" className="button">@jdaviesartwork</a>
     </div>
    );
  }
